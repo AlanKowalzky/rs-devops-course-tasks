e249c6b40d154be52bd4ac79a7c4a7208a45230f
AlanKowalzky <alan.kowalzky@gmail.com>
2025-06-12 23:12:22 +0200

    refactor: workflows gh actions and main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,6 @@
 resource "aws_s3_bucket" "s3_backend_bucket" {
   bucket = var.s3_backend_bucket_name
 
-  # Włączenie wersjonowania dla bezpieczeństwa pliku stanu
-  versioning {
-    enabled = true
-  }
-
-  # Konfiguracja szyfrowania po stronie serwera
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
   # Zapobiega przypadkowemu usunięciu bucketa, jeśli jest to wymagane
   # lifecycle {
   #   prevent_destroy = true
@@ -25,20 +11,24 @@ resource "aws_s3_bucket" "s3_backend_bucket" {
   }
 }
 
-resource "aws_s3_bucket" "application_data_bucket" {
-  bucket = var.application_data_s3_bucket_name
-
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "s3_backend_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_backend_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_backend_bucket_sse" {
+  bucket = aws_s3_bucket.s3_backend_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
   }
+}
+
+resource "aws_s3_bucket" "application_data_bucket" {
+  bucket = var.application_data_s3_bucket_name
 
   # Możesz dodać inne konfiguracje specyficzne dla tego bucketa, np.
   # aws_s3_bucket_public_access_block, aws_s3_bucket_acl, etc.
@@ -46,5 +36,21 @@ resource "aws_s3_bucket" "application_data_bucket" {
   tags = {
     Name        = "Alank Application Data Bucket"
     Environment = "Dev" # Przykładowy tag
+  }
+}
+
+resource "aws_s3_bucket_versioning" "application_data_bucket_versioning" {
+  bucket = aws_s3_bucket.application_data_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "application_data_bucket_sse" {
+  bucket = aws_s3_bucket.application_data_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
