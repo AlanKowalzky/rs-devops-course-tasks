4d7f50743614eb72981df1fec45fc12b11465a74
AlanKowalzky <alan.kowalzky@gmail.com>
2025-06-13 10:28:13 +0200

    fix: iam recu 2

--- iam.tf
+++ iam.tf
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "github_actions_assume_role_policy" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_org_or_user}/${var.github_repo_name}:pull_request"] # Dla Pull Requestów
+      values   = ["repo:${var.github_org_or_user}/${var.github_repo_name}:*"]
     }
 
     # Opcjonalnie, można dodać warunek na audience, jeśli jest to wymagane przez politykę bezpieczeństwa
@@ -29,21 +29,47 @@ resource "aws_iam_role" "github_actions_role" {
   name               = var.github_actions_role_name
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role_policy.json
 
-  # Dołączanie polityk zarządzanych przez AWS
-  # UWAGA: Nadawanie pełnego dostępu (IAMFullAccess) jest ryzykowne dla automatyzacji.
-  # Rozważ zawężenie uprawnień do absolutnie niezbędnych.
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
-    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
-    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/IAMFullAccess", # Bądź ostrożny z tą polityką!
-    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
-    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
-    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
-  ]
-
   tags = {
     Name        = "GitHubActionsRole"
     Description = "IAM role for GitHub Actions to deploy infrastructure via Terraform"
   }
+}
+
+# Dołączanie polityk zarządzanych przez AWS przy użyciu dedykowanego zasobu
+# UWAGA: Nadawanie pełnego dostępu (IAMFullAccess) jest ryzykowne dla automatyzacji.
+# Rozważ zawężenie uprawnień do absolutnie niezbędnych.
+
+resource "aws_iam_role_policy_attachment" "github_actions_ec2_full" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_route53_full" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_s3_full" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_iam_full" { # Bądź ostrożny z tą polityką!
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_vpc_full" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_sqs_full" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_eventbridge_full" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
 }
