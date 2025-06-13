98a11dbe08cb9401d85c5b82607076c891962f2b
AlanKowalzky <alan.kowalzky@gmail.com>
2025-06-13 12:49:43 +0200

    fix: iam debug diag terraform.yml 2

--- iam.tf
+++ iam.tf
@@ -18,11 +18,11 @@ data "aws_iam_policy_document" "github_actions_assume_role_policy" {
     }
 
     # Opcjonalnie, można dodać warunek na audience, jeśli jest to wymagane przez politykę bezpieczeństwa
-    # condition {
-    #   test     = "StringEquals"
-    #   variable = "token.actions.githubusercontent.com:aud"
-    #   values   = ["sts.amazonaws.com"] # Domyślna audience dla AWS
-    # }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"] # Domyślna audience dla AWS
+    }
   }
 }
 
